0b3d1de282ea1bf35b227786e280b34781e0a9d1
FrancescoSuma01 <107925638+FrancescoSuma01@users.noreply.github.com>
2022-12-13 18:14:22 +0100

    Update Cartella.java

--- Tombola/Cartella.java
+++ Tombola/Cartella.java
@@ -1,127 +1,96 @@
-import java.util.Arrays;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class Cartella {
-	private int[] numeri; // numeri contenuti in questa cartella
-	final private boolean[] segnati; // true se il numero corrispondente è segnato
+public class Cartella extends JFrame implements ActionListener{
+    
+    Random nRandom = new Random();
+    JLabel textLabel;
+    JPanel pannello;
+    JButton[][] pulsantiNumeri;
 
+    Cartella(){
 
-	public Cartella() {
-		segnati = new boolean[15]; //default tutti false
-		creaCartella();
-	}
-	
-	// Posiziona i numeri nella cartella
-	private void creaCartella() {
-		numeri = new int[15]; 
-		// Riempio il vettore con 15 numeri casuali che rispettino le regole:
-		// 1. no numeri ripetuti
-		// 2. max 2 numeri con la stessa decina
-		final int[] decine = new int[10]; //indica quanti numeri per ogni decina
-		for (int i=0; i<15; i++) {
-			// Genero un numero casuale tra 1 e 90
-			int n = generaCasuale(1, 90);
-			// Decina attuale
-			final int d = n==90? 8 : n/10; //il 90 va nella colonna degli 80
+        this.setTitle("Cartella - FS"); //titolo della finestra
+        this.setLayout(new BorderLayout()); //imposta come layout il border layout
+        this.setSize(900, 400); //imposta la dimensione della finestra
+        this.setLocationRelativeTo(null); //imposta la posizione della scheda
 
-			// Se il numero casuale generato è già presente oppure se ci sono già due
-			// numeri con la stessa decina, ripeto il calcolo dell'elemento i-esimo
-			if (decine[d] >= 2 || indexOf(n, numeri, i) >= 0) {
-				i--;
-				continue;
-			} else {
-				numeri[i] = n;
-				decine[d]++;
-			}
-		}
+        createLabelCartella();
 
-		// Ordina il vettore finale
-		Arrays.sort(numeri);
+        pannello = new JPanel(); //crea un JPanel che conterrà la tabella
+        pulsantiNumeri = new JButton[3][9]; //crea una matrice di pulsanti
 
-		// Permuta per ottenere le righe finali (un elemento ogni tre nel vettore ordinato)
-		int tmp = numeri[1];
-		numeri[1] = numeri[3];
-		numeri[3] = numeri[9];
-		numeri[9] = numeri[13];
-		numeri[13] = numeri[11];
-		numeri[11] = numeri[5];
-		numeri[5] = numeri[2];
-		numeri[2] = numeri[6];
-		numeri[6] = numeri[4];
-		numeri[4] = numeri[12];
-		numeri[12] = numeri[8];
-		numeri[8] = numeri[10];
-		numeri[10] = numeri[5];
-		numeri[5] = tmp;
-		
-		// Scambia (in verticale) i numeri della stessa colonna se non sono in ordine tra loro
-		for (int i=0; i<15; i++) {
-			final int n = numeri[i];
-			final int d = n/10;
-			for (int j=i; j<15; j++) {
-				final int n2 = numeri[j];
-				final int d2 = n2/10;
-				
-				if (d == d2 && n>n2) { // d==d2: stessa colonna, n>n2 ordine invertito
-					final int temp = numeri[i];
-					numeri[i] = numeri[j];
-					numeri[j] = temp;
-				}
-			}
-		}
-	}
-	
-	// Rappresentazione testuale
-	public void stampa() {
-		String spacer = "   ";
-		for (int r=0; r<3; r++) {
-			String output = "";
-			int d = 0;
-			for (int c=0; c<5; c++) {
-				int index = r*5+c;
-				int num = numeri[index];
+        //riempimento dei 27 bottoni
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                pulsantiNumeri[i][j] = new JButton();
+                pulsantiNumeri[i][j].setFont(new Font("Times New Roman", Font.BOLD, 45));
+                //numbers[i][j].setSize(20, 20);
+                pulsantiNumeri[i][j].setBorder(BorderFactory.createLineBorder(Color.red, 2));
+                pulsantiNumeri[i][j].setOpaque(true);
+                pulsantiNumeri[i][j].setFocusable(false);
+                pulsantiNumeri[i][j].addActionListener(this);
+                pannello.add(pulsantiNumeri[i][j]);
+            }
+        }
 
-				// spazi per i numeri mancanti (per incolonnare i numeri nella giusta decina)
-				int _d = (int)((double)num/10.0);
-				if (num == 90) // il 90 va nella colonna degli 80
-					_d = 8;
-				for (int i=1; i<_d-d+(c==0?1:0); i++)
-					output += spacer+"   ";
-				d = _d;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 3; j++) {
+                int n;
+                do{
+                    n = nRandom.nextInt(0, 10);
+                    n = n == 0 && i == 0 ? n + 1 : n;
+                    n = n + (i * 10);
+                }while(String.valueOf(n).equals(pulsantiNumeri[0][i].getText()) || String.valueOf(n).equals(pulsantiNumeri[1][i].getText()) || String.valueOf(n).equals(pulsantiNumeri[2][i].getText()));
 
-				// stampa numero
-				output += String.format(spacer, num, (segnati[index]?'#':' '));
-			}
+                pulsantiNumeri[j][i].setText(String.valueOf(n));
+            }
+        }
 
-			info(output);
-		}
-	}
-	
-	public static int generaCasuale(final int min, final int max) {
-		return min + (int)(Math.random() * (max-min));
-	}
+        for(int j=0;j<3;j++) {
+            for (int i = 0; i < 4; i++) {
+                int r;
+                do {
+                    r = nRandom.nextInt(0, 9);
+                } while (pulsantiNumeri[j][r].getText() == "");
 
-	// Controlla se un numero è presente in un array e ne restituisce l'indice, o -1 se non presente
-	public static int indexOf(int numero, int[] array, int size) {
-		for (int i = 0; i < size; i++) {
-			if (numero == array[i])
-				return i;
-		}
-		return -1;
-	}
+                pulsantiNumeri[j][r].setText("");
+                pulsantiNumeri[j][r].setEnabled(false);
+            }
+        }
 
-	// Stampa informazioni a schermo
-	public static void info(final String s) {
-		if (true) {
-			String[] lines = s.split("\n\r");
-			for (String l: lines)
-				System.out.println(""+l);
-		}
-	}
+        pannello.setLayout(new GridLayout(3,9));
+        this.add(textLabel,BorderLayout.NORTH);
+        this.add(pannello,BorderLayout.CENTER);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
 
-	// Variante senza argomenti, stampa solo una riga vuota
-	public static void info() {
-		if (true)
-			System.out.println();
-	}
+    public void createLabelCartella(){
+        //crea la label con scritto Cartella in bianco con sfondo rosso
+        textLabel = new JLabel("Cartella", SwingConstants.CENTER);
+        textLabel.setBackground(Color.RED);
+        textLabel.setForeground(Color.WHITE);
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setFont(new Font("Times New Roman", Font.PLAIN, 62));
+        textLabel.setOpaque(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(e.getSource()==pulsantiNumeri[i][j]){
+                    if(pulsantiNumeri[i][j].getBackground()!=(Color.green)){
+                        pulsantiNumeri[i][j].setBackground(Color.green);
+                    }else{
+                        pulsantiNumeri[i][j].setBackground(null);
+                    }
+                }
+            }
+        }
+    }
 }
-
